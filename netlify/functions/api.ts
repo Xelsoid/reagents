35099e735255b1edf2eef605a72427bf2889e75d
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2024-04-09 14:46:55 +0500

    build: add netlify build/deployment  configuration

---
+++ netlify/functions/api.ts
@@ -0,0 +1,4 @@
+import serverless from "serverless-http";
+import { app } from '../../src/app'
+
+export const handler = serverless(app);
---
+++ server.ts
@@ -0,0 +1,6 @@
+import { app } from "./src/app";
+
+app.listen(3000, () => {
+  /* eslint-disable no-console */
+  console.log("Server is started");
+});
