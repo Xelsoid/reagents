c6480c556599699776aeecb7878ff38f7fb22886
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2024-12-10 21:27:50 +0530

    feature: add more user-friendly password field for LogIn modal window

--- client/src/components/LogInModal/index.tsx
+++ client/src/components/LogInModal/index.tsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import { Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 import Alert from '@mui/material/Alert';
 import Button from '@mui/joy/Button';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useUserLogin } from '../../hooks/useLoginUser';
 
 interface ILogInModal {
@@ -10,6 +17,7 @@ interface ILogInModal {
 }
 
 const LogInModal: React.FC<ILogInModal> = ({ isModalShown, closeModal }) => {
+  const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [logInFieldsState, setLogInFieldsState] = useState<{
     name: string | null;
@@ -21,6 +29,10 @@ const LogInModal: React.FC<ILogInModal> = ({ isModalShown, closeModal }) => {
   const [warning, setWarning] = useState<boolean>(false);
 
   const login = useUserLogin();
+
+  const handleClickShowPassword = () => {
+    setShowPassword((prevValue) => !prevValue);
+  };
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLogInFieldsState({
@@ -60,15 +72,29 @@ const LogInModal: React.FC<ILogInModal> = ({ isModalShown, closeModal }) => {
           margin="dense"
           disabled={isLoading}
         />
-        <TextField
-          name="password"
-          label="Пароль"
-          fullWidth
-          value={logInFieldsState.password}
-          onChange={handleOnChange}
-          margin="dense"
-          disabled={isLoading}
-        />
+        <FormControl sx={{ width: '100%' }} variant="outlined" margin="dense">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={showPassword ? 'hide the password' : 'display the password'}
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            name="password"
+            label="Пароль"
+            disabled={isLoading}
+            value={logInFieldsState.password}
+            onChange={handleOnChange}
+          />
+        </FormControl>
       </DialogContent>
       <DialogActions>
         <Button disabled={isLoading} onClick={closeModal}>
